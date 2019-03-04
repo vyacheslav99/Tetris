07901702cf0077b8fb00de09c8693cd6cf237440
balychkov <balychkov@bobday.ru>
2019-03-04 17:55:47 +0300

    Пофиксены баги

--- core/board.py
+++ core/board.py
@@ -49,7 +49,6 @@ class Board(QFrame):
         self.msg2Statusbar.emit(f'Lines: {self.linesRemoved}')
         self.clear()
         self.new_figure()
-        self.update()
         self.timer.start(Board.Speed, self)
 
     def stop(self):
@@ -60,7 +59,7 @@ class Board(QFrame):
         self.isStarted = False
         self.isPaused = False
         self.locked = False
-        self.msg2Statusbar.emit(f'End. Lines: {self.linesRemoved}')
+        self.msg2Statusbar.emit(f'Game Over! Lines: {self.linesRemoved}')
         self.update()
 
     def pause(self):
@@ -71,9 +70,10 @@ class Board(QFrame):
 
         if self.isPaused:
             self.timer.stop()
-            self.msg2Statusbar.emit('-= paused =-')
+            self.msg2Statusbar.emit('-= PAUSED =-')
             self.update()
         else:
+            self.msg2Statusbar.emit(f'Lines: {self.linesRemoved}')
             self.timer.start(Board.Speed, self)
 
     def scale_width(self):
@@ -84,9 +84,10 @@ class Board(QFrame):
         """ масштабирование - рассчитывает размер стороны квадрата в пикселях по оси Y (высота) """
         return self.contentsRect().height() // Board.BHeight
 
-    def calc_pos(self, centerTop, centerLeft):
+    def calc_pos(self, centerTop, centerLeft, prew_pos=False):
         """ вычисляет реальные координаты всех точек фигуры на доске от заданной точки центра по смещениям фигуры """
-        return [[centerTop + offset[0], centerLeft + offset[1]] for offset in self.figure.offsets]
+        offsets = self.figure.prew_offsets if prew_pos and self.figure.prew_offsets else self.figure.offsets
+        return [[centerTop + offset[0], centerLeft + offset[1]] for offset in offsets]
 
     def calc_extremums(self, positions):
         """ вычисляет крайние значения набора координат """
@@ -106,8 +107,8 @@ class Board(QFrame):
     def check_pos(self, pos):
         """ Проверяет, свободны ли на доске точки с заданными координатами """
         for coord in pos:
-            if coord[0] < 0 or coord[0] >= Board.BHeight or coord[1] < 0 or coord[1] >= Board.BWidth or (
-                coord not in pos and self.board[coord[0]][coord[1]] != Figure.FIG_TYPE_NONE):
+            if coord[0] < 0 or coord[0] >= Board.BHeight or coord[1] < 0 or coord[1] >= Board.BWidth \
+                or self.board[coord[0]][coord[1]] < 0:
                 return False
 
         return True
@@ -138,15 +139,24 @@ class Board(QFrame):
         if not self.check_pos(pos):
             self.stop()
 
+        for coord in pos:
+            self.board[coord[0]][coord[1]] = self.figure.fig_type
+
+        self.update()
+
+    def anchor(self, pos):
+        for coord in pos:
+            self.board[coord[0]][coord[1]] = -self.board[coord[0]][coord[1]]
+
     def remove_line(self, rowNo):
         """ стирание переданной линии и сдвиг всего, что выше вниз на одну линию """
         self.linesRemoved += 1
 
-        canStop = True
+        for row in range(rowNo, 0, -1):
+            canStop = True
 
-        for row in range(rowNo, -1, -1):
             for col in range(len(self.board[row])):
-                self.board[row][col] = self.board[row][col] - 1
+                self.board[row][col] = self.board[row-1][col]
                 canStop = canStop and self.board[row][col] == Figure.FIG_TYPE_NONE
 
             if canStop:
@@ -190,9 +200,11 @@ class Board(QFrame):
                 pass
 
             self.locked = True
-            pos = self.calc_pos(newRow, newCol)
+            new_pos = self.calc_pos(newRow, newCol)
+            curr_pos = self.calc_pos(self.figRow, self.figCol,
+                                     prew_pos=(newCol == self.figCol and newRow == self.figRow))
 
-            if not self.check_pos(pos):
+            if not self.check_pos(new_pos):
                 # проверим, не вылезли ли новые координаты за левые/правые стенки или не воткнулись ли мы в
                 # торчащую где-то на доске занятую клетку, если да, то:
                 if newCol != self.figCol or (newCol == self.figCol and newRow == self.figRow):
@@ -204,16 +216,18 @@ class Board(QFrame):
                     return False
                 else:
                     # это было движение вниз:
-                    # проверяем и стираем полные линии,
+                    # фиксируем фигуру на полу
+                    # проверяем и стираем полные линии
                     # затем создаем новую фигуру (т.е. переходим к новому циклу игры)
-
+                    self.anchor(curr_pos)
                     self.clear_full_lines(Board.BHeight - 1)
                     self.new_figure()
                     return False
 
             # все ОК, можно двигать:
             # очищаем по текущим координатам точки на доске
-            for coord in self.calc_pos(self.figRow, self.figCol):
+            for coord in self.calc_pos(self.figRow, self.figCol,
+                                       prew_pos=(newCol == self.figCol and newRow == self.figRow)):
                 self.board[coord[0]][coord[1]] = Figure.FIG_TYPE_NONE
 
             # фиксируем новые координаты цетра фигуры
@@ -221,7 +235,7 @@ class Board(QFrame):
             self.figCol = newCol
 
             # заполняем по новым координатам точки на доске флагом типа фигуры
-            for coord in pos:
+            for coord in new_pos:
                 self.board[coord[0]][coord[1]] = self.figure.fig_type
 
             return True
@@ -237,7 +251,7 @@ class Board(QFrame):
     def paintEvent(self, event):
         for i in range(Board.BHeight):
             for j in range(Board.BWidth):
-                self.draw_square(j * self.scale_width(), i * self.scale_height(), self.board[i][j])
+                self.draw_square(j * self.scale_width(), i * self.scale_height(), abs(self.board[i][j]))
 
     def keyPressEvent(self, event):
         key = event.key()
